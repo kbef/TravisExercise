d78415c01d02f50c0c11a07203f08e7f004a9eb6
Heng Li <henglicad@gmail.com>
2020-01-27 00:37:28 -0800

    added a bug in Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,7 @@ public class Demo {
 
   public static void main(String argv[]) {
     Order order = new Order("Shane", 444.0f);
+    Order order = new Order("Shane", 444.0f, "bug");
     DiscountStrategy strategy = null;
 
     int selectedStrat = Integer.parseInt(argv[0]);
